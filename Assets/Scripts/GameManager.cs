5f3b7b83f787673bd042b590316675c1e2976f4d
GuilhermeCSoraggi <guilhermecabellosoraggi@hotmail.com>
2025-11-02 00:38:27 -0300

    Prefab textures
    Prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public void WinScene()
     {
-        
+        SceneManager.LoadScene("WinScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoseScene()
     {
-        
+        SceneManager.LoadScene("LoseScene");
     }
 }
